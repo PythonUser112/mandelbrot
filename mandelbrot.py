aeaaede67aee77ff0079db65a7bc2f8090aefae8
PythonUser112 <buddha@sbs-braungardt.de>
2025-03-29 16:18:57 +0100

    Added benchmarking functionality

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,6 +6,7 @@ Created on Sat Feb  8 16:04:18 2025
 @author: buddha
 """
 
+import time
 import sys
 import math
 import PIL.Image
@@ -14,13 +15,14 @@ STARTX = -2
 STARTY = -1.5
 ENDX = 1
 ENDY = 1.5
-STEP = 0.0005
+STEP = 0.01
 SIZE = 2
 BORDER = 2
 CHARACTER = "*"
 FOREGROUND = True
 UPRIGHT = False
-EXPORT = "mandelbrot_0.0005.png"
+EXPORT = ""
+BENCHMARK = False
 
 if len(sys.argv) > 1:
     if len(sys.argv) == 5:
@@ -64,9 +66,24 @@ if len(sys.argv) > 1:
             CHARACTER = "\u2588"
         UPRIGHT = True if sys.argv[9] == "1" else False
         EXPORT = sys.argv[10]
+    elif len(sys.argv) == 12:
+        STARTX = float(sys.argv[1])
+        STARTY = float(sys.argv[2])
+        ENDX = float(sys.argv[3])
+        ENDY = float(sys.argv[4])
+        STEP = float(sys.argv[5])
+        SIZE = int(sys.argv[6])
+        BORDER = float(sys.argv[7])
+        CHARACTER = sys.argv[8]
+        if CHARACTER == " ":
+            CHARACTER = "\u2588"
+        UPRIGHT = True if sys.argv[9] == "1" else False
+        EXPORT = sys.argv[10]
+        BENCHMARK = bool(sys.argv[11])
     else:
         print(f"Usage: python3 {sys.argv[0]} [<startx> <starty> <endx> <endy> [<step>"
-              " <pixelsize> <colorborder> <character> [<upright> [<export_to>]]]]")
+              " <pixelsize> <colorborder> <character> [<upright> [<export_to>"
+              "[<benchmark>]]]]")
         sys.exit(1)
 
 colors = [[255, 255, 255], [255, 0, 0], [255, 64, 0], [255, 128, 0], 
@@ -100,6 +117,8 @@ def get_value(real, imag, cimag, creal, border, depht=(len(colors) - 1)):
         return 0
 
 if not EXPORT:
+    if BENCHMARK:
+        time_start = time.time()
     if not UPRIGHT:
         for imag in frange(STARTY, ENDY, STEP):
             for real in frange(STARTX, ENDX, STEP):
@@ -112,18 +131,27 @@ if not EXPORT:
                 value = get_value(0, 0, imag, real, BORDER)
                 print(colorize(CHARACTER*SIZE, *colors[value]), end="")
             print()
+    if BENCHMARK:
+        duration = time.time() - time_start
+        pixels = int((ENDX-STARTX)/STEP) * int((ENDY-STARTY)/STEP)
+        print("Rendered", pixels, "pixels in", round(duration, 5), "seconds.")
+    
 else:
     image = PIL.Image.new("RGB", (int((ENDX-STARTX)/STEP), int((ENDY-STARTY)/STEP)))
+    if BENCHMARK:
+        time_start = time.time()
     if not UPRIGHT:
         for imag in frange(STARTY, ENDY, STEP):
             for real in frange(STARTX, ENDX, STEP):
                 value = get_value(0, 0, imag, real, BORDER)
                 image.putpixel((int((real-STARTX)/STEP), int((imag-STARTY)/STEP)), tuple(colors[value]))
-            print(str(round(100 - (ENDY-imag)/(ENDY-STARTY)*100, 10)).ljust(13, "0"), "% (", str(round(imag, 4)).ljust(7, "0"), "/", ENDY, ")", sep="", end="\r")
     else:
         for real in frange(STARTX, ENDX, STEP):
             for imag in frange(STARTY, ENDY, STEP):
                 value = get_value(0, 0, imag, real, BORDER)
                 image.putpixel((int((real-STARTX)/STEP), int((imag-STARTY)/STEP)), tuple(colors[value]))
-            print(str(round(100 - (ENDX-real)/(ENDX-STARTX)*100, 10)).ljust(13, "0"), "% (", str(round(real, 4)).ljust(7, "0"), "/", ENDX, ")", sep="", end="\r")
+    if BENCHMARK:
+        duration = time.time() - time_start
+        pixels = int((ENDX-STARTX)/STEP) * int((ENDY-STARTY)/STEP)
+        print("Rendered", pixels, "pixels in", round(duration, 5), "seconds.")
     image.save(EXPORT, mode="png")
